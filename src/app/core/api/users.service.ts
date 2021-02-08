5c76471218df00a52b80d49a09769b5bae5f19aa
ANovokmet <ante.je.zakon@gmail.com>
2021-02-09 00:34:13 +0100

    remove my token

--- src/app/core/api/users.service.ts
+++ src/app/core/api/users.service.ts
@@ -12,7 +12,6 @@ export class UsersService {
 
   searchUsers(query: string, page: number = 0, perPage: number = 10): Observable<ItemsResult<User>> {
     return this.http.get<ItemsResult<User>>(`${environment.githubApi}/search/users`, {
-      headers: { Authorization: `token 278ba7728896d535159e13ce35976d817a8c8f3c`},
       params: {
         q: query,
         page: page.toString(),
@@ -22,12 +21,11 @@ export class UsersService {
   }
 
   getUser(login: string): Observable<UserDetail> {
-    return this.http.get<UserDetail>(`${environment.githubApi}/users/${login}`, { headers: { Authorization: `token 278ba7728896d535159e13ce35976d817a8c8f3c`}});
+    return this.http.get<UserDetail>(`${environment.githubApi}/users/${login}`);
   }
 
   getUserFollowers(login: string, page: number = 0, perPage: number = 10): Observable<User[]> {
     return this.http.get<User[]>(`${environment.githubApi}/users/${login}/followers`, {
-      headers: { Authorization: `token 278ba7728896d535159e13ce35976d817a8c8f3c`},
       params: {
         page: page.toString(),
         per_page: perPage.toString()
